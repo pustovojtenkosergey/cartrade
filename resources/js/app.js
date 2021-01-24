4d7dbfa4d21d0b886b3004df6a7c8101e3095cb4
spustovojtenko <spustovoytenko@xmlshop.com>
2021-01-24 13:00:23 +0200

    chat+

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,22 @@
 require('./bootstrap');
 
-import Vue from 'vue'
+// Import modules...
+import Vue from 'vue';
+import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue';
+import PortalVue from 'portal-vue';
 
-import App from "./vue/app.vue"
+Vue.mixin({ methods: { route } });
+Vue.use(InertiaPlugin);
+Vue.use(PortalVue);
 
-const app = new Vue({
-    el: '#app',
-    components: { App }
-})
+const app = document.getElementById('app');
+
+new Vue({
+    render: (h) =>
+        h(InertiaApp, {
+            props: {
+                initialPage: JSON.parse(app.dataset.page),
+                resolveComponent: (name) => require(`./Pages/${name}`).default,
+            },
+        }),
+}).$mount(app);
---
+++ resources/js/vue/listView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div v-for="(item, index) in items" :key="index">
+            <list-item
+                :item="item"
+                class="item"
+                v-on:itemchanged="$emit('reloadList')"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import ListItem from "./listItem";
+export default {
+    components: {ListItem},
+    props: [
+        'items'
+    ]
+}
+</script>
+
+<style>
+    .item {
+        background: #e6e6e6;
+        padding: 5px;
+        margin-top: 5px;
+    }
+</style>
